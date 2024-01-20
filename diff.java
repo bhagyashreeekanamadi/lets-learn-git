1a91c8867f0667165cf6dcd0bb018d4cdee731a3
Bhagyashree Kanamadi <kanamadi.bhagyashree.8@gmail.com>
2024-01-20 07:38:56 +0530

    adding diff.java

---
+++ diff.java
@@ -0,0 +1,10 @@
+public class diff {
+    public static void main(String[] args) {
+    
+
+        int a=9;
+        int b=1;
+        int r=a-b;
+        System.out.println("the difference is: "+r);
+    }
+}
